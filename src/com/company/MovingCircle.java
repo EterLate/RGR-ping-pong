68a2cbf02d2b0afed83d5aa29127c53a8abd323c
EterLate <52948550+EterLate@users.noreply.github.com>
2020-06-12 22:41:17 +0600

    add new functions
    add some new functions

--- src/com/company/MovingCircle.java
+++ src/com/company/MovingCircle.java
@@ -2,6 +2,7 @@ package com.company;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
 import javax.swing.*;
 
 
@@ -11,8 +12,11 @@ public class MovingCircle extends JComponent implements ActionListener {
     private Timer timer;
     public int scopeleft=0;
     public int scoperight=0;
-    public double x =10;
-    public double y =60;
+    public double x =300;
+    public double y =200;
+    public double playballsize = 20;
+    public double xa=x-playballsize;
+    public double ya=y-playballsize;
     private double basespeed=3;
     public double speed=basespeed;
     public double changex=speed;
@@ -27,6 +31,18 @@ public class MovingCircle extends JComponent implements ActionListener {
     public boolean DownPressed=false;
     public boolean WPressed=false;
     public boolean SPressed=false;
+    public int bonusballx = 550;
+    public int bonusbally = 275;
+    public int bonusballsize = 30;
+    public int debuffballx = 550;
+    public int debuffbally = 100;
+    public int debuffballsize = 30;
+    public int darkballx = 550;
+    public int darkbally = 450;
+    public int darkballsize = 30;
+    public boolean BlackSquare = false;
+    public int BlackSquarex;
+    public int BlackSquarey;
 
     public MovingCircle(Color color, int delay) {
         scale = 1.0;
@@ -35,7 +51,7 @@ public class MovingCircle extends JComponent implements ActionListener {
 
         setPreferredSize(new Dimension(1100, 500));
     }
- public void start() {
+    public void start() {
         timer.start();
     }
 
@@ -46,9 +62,9 @@ public class MovingCircle extends JComponent implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent arg0) {
         if (UpPressed && RightCoord>50) RightCoord-=5;
-        if (DownPressed && RightCoord<400) RightCoord+=5;
+        if (DownPressed && RightCoord<500 - RightPlateSizeY) RightCoord+=5;
         if (WPressed && LeftCoord>50) LeftCoord-=5;
-        if (SPressed && LeftCoord<400) LeftCoord+=5;
+        if (SPressed && LeftCoord<500 - RightPlateSizeY) LeftCoord+=5;
         repaint();
 
     }
@@ -59,6 +75,7 @@ public class MovingCircle extends JComponent implements ActionListener {
         g2d.setColor(Color.white);
         int width = 1100;
         int height = 450;
+        Random rand = new Random();
         g.fillRect(0,50,width,height);
         g2d.setColor(Color.black);
         g2d.drawRect(0, 50, width - 1, height - 1);
@@ -74,17 +91,56 @@ public class MovingCircle extends JComponent implements ActionListener {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(color);
         g2d.scale(scale, scale);
-        if(LeftCoord<y && (LeftCoord+100) >y && x<70 && x>50){
+        xa=x+playballsize/2;
+        ya=y+playballsize/2;
+        if ((((xa+((bonusballsize / 2) + (playballsize / 2)))>bonusballx && x<bonusballx) && (ya>=bonusbally && ya<=(bonusbally+((bonusballsize / 2) + (playballsize / 2)))))
+                || (((xa+((bonusballsize / 2) + (playballsize / 2)))>bonusballx && xa<bonusballx) && (ya<=bonusbally && (ya+((bonusballsize / 2) + (playballsize / 2)))>=bonusbally))
+                || ((xa<(bonusballx+((bonusballsize / 2) + (playballsize / 2))) && xa>bonusballx) && (ya>=bonusbally && ya<=(bonusbally+((bonusballsize / 2) + (playballsize / 2)))))
+                || ((xa<(bonusballx+((bonusballsize / 2) + (playballsize / 2))) && xa>bonusballx) && (ya<=bonusbally && (ya+((bonusballsize / 2) + (playballsize / 2)))>=bonusbally)))
+        {
+            bonusballx = rand.nextInt(900-bonusballsize)+100;
+            bonusbally = rand.nextInt(400-bonusballsize)+50;
+            LeftPlateSizeY += 25;
+            RightPlateSizeY += 25;
+        }
+        if ((((xa+((debuffballsize / 2) + (playballsize / 2)))>debuffballx && x<debuffballx) && (ya>=debuffbally && ya<=(debuffbally+((debuffballsize / 2) + (playballsize / 2)))))
+                || (((xa+((debuffballsize / 2) + (playballsize / 2)))>debuffballx && xa<debuffballx) && (ya<=debuffbally && (ya+((debuffballsize / 2) + (playballsize / 2)))>=debuffbally))
+                || ((xa<(debuffballx+((debuffballsize / 2) + (playballsize / 2))) && xa>debuffballx) && (ya>=debuffbally && ya<=(debuffbally+((debuffballsize / 2) + (playballsize / 2)))))
+                || ((xa<(debuffballx+((debuffballsize / 2) + (playballsize / 2))) && xa>debuffballx) && (ya<=debuffbally && (ya+((debuffballsize / 2) + (playballsize / 2)))>=debuffbally)))
+        {
+            debuffballx = rand.nextInt(900-debuffballsize)+100;
+            debuffbally = rand.nextInt(400-debuffballsize)+50;
+            LeftPlateSizeY -= 25;
+            RightPlateSizeY -= 25;
+        }
+        if ((((xa+((darkballsize / 2) + (playballsize / 2)))>darkballx && x<darkballx) && (ya>=darkbally && ya<=(darkbally+((darkballsize / 2) + (playballsize / 2)))))
+                || (((xa+((darkballsize / 2) + (playballsize / 2)))>darkballx && xa<darkballx) && (ya<=darkbally && (ya+((darkballsize / 2) + (playballsize / 2)))>=darkbally))
+                || ((xa<(darkballx+((darkballsize / 2) + (playballsize / 2))) && xa>darkballx) && (ya>=darkbally && ya<=(darkbally+((darkballsize / 2) + (playballsize / 2)))))
+                || ((xa<(darkballx+((darkballsize / 2) + (playballsize / 2))) && xa>darkballx) && (ya<=darkbally && (ya+((darkballsize / 2) + (playballsize / 2)))>=darkbally)))
+        {
+            darkballx = rand.nextInt(900-darkballsize)+100;
+            darkbally = rand.nextInt(400-darkballsize)+50;
+            BlackSquarex = rand.nextInt(600)+200;
+            BlackSquarey = rand.nextInt(50)+200;
+            if (BlackSquare == false){
+                BlackSquare = true;
+            } else {
+                BlackSquare = false;
+            }
+        }
+        if(LeftCoord<y && (LeftCoord+LeftPlateSizeY) >y && x<70 && x>50){
             speed+=1;
             changex=speed;
 
         }
-        if(RightCoord<y && (RightCoord+100) >y && x>1030 && x<1050){
+        if(RightCoord<y && (RightCoord+RightPlateSizeY) >y && x>1030 && x<1050){
             speed+=1;
             changex=-speed;
         }
-                //правая граница
+        //правая граница
         if(x>1080){
+            BlackSquare = false;
+            RightPlateSizeY=100;
             scopeleft+=1;
             x=550;
             y=250;
@@ -94,6 +150,8 @@ public class MovingCircle extends JComponent implements ActionListener {
         }
         //левая граница
         if(x<0){
+            BlackSquare = false;
+            LeftPlateSizeY=100;
             scoperight+=1;
             x=550;
             y=250;
@@ -114,8 +172,24 @@ public class MovingCircle extends JComponent implements ActionListener {
 
         x=x+changex;
         y=y+changey;
-        Ellipse2D el = new Ellipse2D.Double(x, y, 20, 20);
+        Ellipse2D el = new Ellipse2D.Double(x, y, playballsize, playballsize);
         g2d.fill(el);
+        g2d.setColor(Color.orange);
+        Ellipse2D bonusball = new Ellipse2D.Double(bonusballx-bonusballsize/2, bonusbally-bonusballsize/2, bonusballsize, bonusballsize);
+        g2d.fill(bonusball);
+        g2d.setColor(Color.pink);
+        Ellipse2D debuffball = new Ellipse2D.Double(debuffballx-debuffballsize/2, debuffbally-debuffballsize/2, debuffballsize, debuffballsize);
+        g2d.fill(debuffball);
+        if (BlackSquare == true){
+            g2d.setColor(Color.black);
+            g2d.fillRect(BlackSquarex, BlackSquarey, 200, 200);
+            g2d.setColor(Color.cyan);
+        } else {
+            g2d.setColor(Color.black);
+        }
+        Ellipse2D darkball = new Ellipse2D.Double(darkballx-darkballsize/2, darkbally-darkballsize/2, darkballsize, darkballsize);
+        g2d.fill(darkball);
+
     }
 
 
